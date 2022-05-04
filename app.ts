cfbd7ea44a043a28bdc2fe0606b82582a7074cec
= <adedejifavour6@gmail.com>
2022-05-04 11:59:36 +0400

    source file for drag and drop project in typescript

---
+++ app.ts
@@ -0,0 +1,16 @@
+// /// <reference path="component/project-input.ts" />
+// /// <reference path="component/project-list.ts" />
+
+import { ProjectInput } from "./component/project-input";
+import { ProjectList } from "./component/project-list";
+
+// namespace App {
+ new ProjectInput()
+
+ new ProjectList("active")
+ new ProjectList("finished")
+// } 
+
+console.log("Hi");
+
+ 
